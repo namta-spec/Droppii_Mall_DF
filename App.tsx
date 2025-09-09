963c0c0adc5992500b114286e461bf1e7c856645
DangNam <nam.ta@digitalfortress.dev>
2025-09-09 16:48:51 +0700

    feat: Config Context API

--- App.tsx
+++ App.tsx
@@ -6,29 +6,37 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Button, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import "./global.css"
+import AppProvider, { AppContext, AppContextType } from 'context/AppContext';
+import { useContext } from 'react';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <AppProvider>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </SafeAreaProvider>
+    </AppProvider>
   );
 }
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const {isDarkMode, setIsDarkMode} = useContext(AppContext) as AppContextType;
+
+  console.log(isDarkMode ? 'light-content' : 'dark-content');
 
   return (
-    <View style={styles.container}>     
+    <View style={styles.container}>  
+    <Button title="Tap to switch" onPress={() => setIsDarkMode(!isDarkMode)}/>   
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
